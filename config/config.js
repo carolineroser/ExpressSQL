12c8e75876f292a9ec6b866915f3830731a8a50a
Caroline Tipton <croser@ksu.edu>
2019-11-05 10:36:17 -0600

    34 min mark in video

---
+++ config/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "development": {
+    "username": "brmiyaqx",
+    "password": "B0L9kTxBr0LmWIM0XcHOs8NU0lEZlwkr",
+    "database": "brmiyaqx",
+    "host": "salt.db.elephantsql.com",
+    "dialect": "postgress",
+    "port": 5432,
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  }
+}
