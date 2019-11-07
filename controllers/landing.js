a40ed3fd6ed01ecce51e9538a72a47e2f06f71e1
Caroline Tipton <croser@ksu.edu>
2019-11-07 14:41:48 -0600

    Hour and five min mark in video

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,20 +1,51 @@
 const models = require('../models')
 
-exports.get_landing=function(req, res, next) {
-    res.render('landing', { title: 'Express' });
+exports.get_landing = function(req, res, next) {
+  res.render('landing', { title: 'Express', user: req.user });
 }
 
-exports.submit_lead=function(req, res, next) {
-    console.log("lead email:", req.body.lead_email);
-    return models.lead.create({
-        email: req.body.lead_email
-    }).then(lead => {
-        res.redirect('/leads');
-    })
+exports.submit_lead = function(req, res, next) {
+	return models.Lead.create({
+		email: req.body.lead_email
+	}).then(lead => {
+		res.redirect('/leads');
+	})
 }
 
 exports.show_leads = function(req, res, next) {
 	return models.Lead.findAll().then(leads => {
- 	res.render('lead/leads', { title: 'Express', leads: leads });		
+ 		res.render('landing', { title: 'Express', leads: leads });		
 	})
+}
+
+exports.show_lead = function(req, res, next) {
+    return models.Lead.findOne({
+        where : {
+            id : req.params.lead_id
+        }
+    }).then(lead =>{
+        res.render('lead', {lead : lead});
+    })
+}
+
+exports.show_edit_lead = function(req, res, next) {
+    return models.Lead.findOne({
+        where : {
+            id : req.params.lead_id
+        }
+    }).then(lead =>{
+        res.render('lead/edit_lead', {lead : lead});
+    })
+}
+
+exports.edit_lead = function(req, res, next) {
+    return models.Lead.update({
+        email: req.body.lead_email
+    }, {
+        where:{
+            id: req.params.lead_id
+        }
+    }).then(results =>{
+        res.redirect('/lead/' + req.params.lead_id);
+    })
 }
